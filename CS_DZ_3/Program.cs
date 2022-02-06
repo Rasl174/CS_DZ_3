f60ca5eca5d038784a1dc8f74ba6a2ea80bf90bf
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-06 10:48:37 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOldLadies;
+            int time;
+
+            Console.Write("Введите количество старушек: ");
+            numberOfOldLadies = Convert.ToInt32(Console.ReadLine());
+
+            time = numberOfOldLadies * 10;
+
+            int chas = time / 60;
+            int min = time % 60;
+
+            Console.Write("Вы должны остстоять в очереди " + chas + " часа и " + min + " минут ");
+
+        }
+    }
+}
